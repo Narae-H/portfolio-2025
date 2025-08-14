59e812d460cbda7629944a6e570a293ec9eac098
NaraeH <79120733+Narae-H@users.noreply.github.com>
2025-08-14 22:03:56 +1000

    fix: resolve next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-const nextConfig: NextConfig = {  
-  basePath: isProd ? '/portfolio-2025' : '',     
-  assetPrefix: isProd ? '/portfolio-2025/' : '', 
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
